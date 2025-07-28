856a33874f59b5521f858dc766b755c225a375df
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-07-29 00:39:45 +0300

    refactored structure

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,27 @@ public class Main {
         // Upload some Egyptian photos
         manager.uploadPhoto(new Photo("1", "pyramids.jpg", LocalDate.of(2023, 3, 10),
                 new Coordinates(25, 30), Set.of("pyramids", "history", "egypt", "tourism")));
-
         manager.uploadPhoto(new Photo("2", "nile.jpg", LocalDate.of(2023, 4, 22),
                 new Coordinates(50, 75), Set.of("nile", "river", "sunset", "egypt")));
-
         manager.uploadPhoto(new Photo("3", "mosque.jpg", LocalDate.of(2023, 4, 22),
                 new Coordinates(100, 50), Set.of("mosque", "islamic", "architecture", "egypt")));
-
         manager.uploadPhoto(new Photo("4", "museum.jpg", LocalDate.of(2023, 5, 15),
                 new Coordinates(35, 45), Set.of("museum", "antiquities", "egypt", "pharaohs")));
 
         // Search by single tag
         System.out.println("Photos with tag 'egypt':");
         manager.searchByTag("egypt").forEach(System.out::println);
+        System.out.println();
 
         // Search by date
         System.out.println("Photos taken on 2023-04-22:");
         manager.searchByDate(LocalDate.of(2023, 4, 22)).forEach(System.out::println);
+        System.out.println();
 
         // Search by location
         System.out.println("Photos taken in Cairo:");
         manager.searchByLocation("Cairo").forEach(System.out::println);
+        System.out.println();
 
         // Search by multiple tags
         System.out.println("Photos with tags [egypt, museum]:");
